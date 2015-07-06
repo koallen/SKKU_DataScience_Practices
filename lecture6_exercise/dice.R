23f27319ee16871243740a5a3d79bbd145138dbb
koallen <weifengzi2009@gmail.com>
2015-07-06 10:11:27 +0900

    add lecture 6 exercise

---
+++ lecture6_exercise/dice.R
@@ -0,0 +1,23 @@
+# Lecture 6 exercise
+# Rolling the dice
+#
+# Siyuan Liu
+# 06/07/2015
+
+# 10 experiments
+r10_1 <- sample(1:6, 10, replace=T)
+r10_2 <- sample(1:6, 10, replace=T)
+r10 <- r10_1 + r10_2
+hist(r10)
+
+# 100 experiments
+r100_1 <- sample(1:6, 100, replace=T)
+r100_2 <- sample(1:6, 100, replace=T)
+r100 <- r100_1 + r100_2
+hist(r100)
+
+# 10000 experiments
+r10000_1 <- sample(1:6, 10000, replace=T)
+r10000_2 <- sample(1:6, 10000, replace=T)
+r10000 <- r10000_1 + r10000_2
+hist(r10000)
